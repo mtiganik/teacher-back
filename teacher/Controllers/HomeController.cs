8246d8dda09b43789e90b0678123467019590f70
Mihkel Tiganik <mtiganik@gmail.com>
2023-01-24 03:00:45 +0200

    testcontroller

---
+++ teacher/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace teacher.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "hello", "World" };
+        }
+    }
+}
